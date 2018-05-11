4e2ec9c9a8ba3fd735d2b8a55acf298465141a0e
Katerina Rubtsova <katerinarubtsova@mail.ru>
2018-05-11 17:30:17 +0300

    адаптивная графика, работа над соответствием макету

--- source/js/nav-menu.js
+++ source/js/nav-menu.js
@@ -1,6 +1,8 @@
 var navButton = document.querySelector(".main-nav__button");
 var navMenu = document.querySelector(".site-list");
 var navColor = document.querySelector(".main-nav");
+var pageHeaderMain = document.querySelector(".page-header--main");
+var pageHeaderRegular = document.querySelector(".page-header--regular");
 
 navButton.addEventListener("click", function() {
   if (navButton.classList.contains("main-nav__button--burger")) {
@@ -10,6 +12,8 @@ navButton.addEventListener("click", function() {
     navMenu.classList.add("site-list--opened");
     navColor.classList.remove("main-nav--transparent");
     navColor.classList.add("main-nav--filled");
+    pageHeaderMain.classList.add("page-header--opened-main");
+    pageHeaderRegular.classList.add("page-header--opened-regular");
   }
   else {
     navButton.classList.remove("main-nav__button--cross");
@@ -18,5 +22,7 @@ navButton.addEventListener("click", function() {
     navMenu.classList.add("site-list--closed");
     navColor.classList.remove("main-nav--filled");
     navColor.classList.add("main-nav--transparent");
+    pageHeaderMain.classList.remove("page-header--opened-main");
+    pageHeaderRegular.classList.remove("page-header--opened-regular");
   }
 });
